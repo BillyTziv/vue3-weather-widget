2fbc0e83b8d55f7864ee79105a6d85b41d2b6125
Billy Tziv <vtzivaras@gmail.com>
2023-06-16 12:39:03 +0300

    Pinua Setup

---
+++ src/stores/weatherStore.js
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia';
+
+const baseURL = 'https://api.openweathermap.org';
+
+export const useWeatherStore = defineStore('weather', {
+  state: () => ({
+    weatherData: null,
+    error: null,
+  }),
+  actions: {
+    async fetchWeatherData() {
+      try {
+        const response = await fetch(`${baseURL}/data/2.5/onecall?lat=40.58725980318928&lon=22.948223362612612&exclude=hourly,minutely&appid=11b0499bd13ab56063de7565a440eb97&units=metric`);
+        const data = await response.json();
+        this.weatherData = data;
+      } catch (error) {
+        this.error = error.message;
+      }
+    },
+  },
+});
