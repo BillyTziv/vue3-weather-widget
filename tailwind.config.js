3c8ce88bec800863b989a283ba9c2c5f73c2fe28
Billy Tziv <vtzivaras@gmail.com>
2023-06-16 12:38:42 +0300

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: [
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+}
+
